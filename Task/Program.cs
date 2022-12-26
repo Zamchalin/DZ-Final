20b0b86a1186c48c0c1ec4f903237961cf4f9c9f
Michael <zamchalin@mail.ru>
2022-12-26 22:17:42 +0300

    Added dz

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arrayinput = new string[5] {"544", ":)", "hello", "world", "hi"};
+string[] result = new string[arrayinput.Length];
+void SecondArrayWithIF(string[] arrayinput, string[] result)
+{
+    int count = 0;
+    for (int i = 0; i < arrayinput.Length; i++)
+    {
+    if(arrayinput[i].Length <= 3)
+        {
+        result[count] = arrayinput[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Console.Clear();
+SecondArrayWithIF(arrayinput, result);
+PrintArray(result);
